b1286ac33895041c47154ed3febdc737cd069580
Silvestre <silvestrevideoproducoes@gmail.com>
2021-05-03 15:04:54 -0300

    corrigindo variáveis

--- JS_calcularIdade.js
+++ JS_calcularIdade.js
@@ -1,10 +1,10 @@
 alert("Seja Bem Vindo!")
 
-let nameVelha = prompt("Qual o nome da pessoa mais velha?")
+let nameOld = prompt("Qual o nome da pessoa mais velha?")
 
-let ageVelha = prompt("Qual a idade da pessoa mais velha?")
+let ageOld = prompt("Qual a idade da pessoa mais velha?")
 
-alert("Obrigado " + nameVelha)
+alert("Obrigado " + nameOld)
 
 let nameNew = prompt("Qual o nome da pessoa mais nova?")
 
@@ -12,11 +12,11 @@ let ageNew = prompt("Qual a idade da pessoa mais nova")
 
 alert("Obrigado " + nameNew)
 
-let diferetAge =  (ageVelha - ageNew)
+let diferetAge =  (ageOld - ageNew)
 
 alert(
-    nameVelha + " você é a pessoa mais velha!" + 
-    "\nIdade: " + ageVelha + " anos\n\n" + nameNew + " você é a pessoa mais nova!" + "\nIdade: " + ageNew + " anos\n\n" + 
+    nameOld + " você é a pessoa mais velha!" + 
+    "\nIdade: " + ageOld + " anos\n\n" + nameNew + " você é a pessoa mais nova!" + "\nIdade: " + ageNew + " anos\n\n" + 
     "A diferença de idade entre as duas pessoas é de " + diferetAge + " anos")
 
     //o resultado será o nome e idade da pessoa mais velha e da mais nova e a difença de idade entre elas
